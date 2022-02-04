e56b6a3255a6e3207d83c1ce67b2d86f19a6572f
Iurrii <90149305+Iurrii@users.noreply.github.com>
2022-02-04 01:14:10 +0100

    added round to 0.05
    Добавлено округление с точностью до 0.05 для  randomSum (сумма покупки) перед вычислением сдачи.
    Добавлены комментарии.

--- script.js
+++ script.js
@@ -19,15 +19,39 @@ getDataAttributesFromElementsOfArray(getArrayFromNodeList(ifElements), ifValues)
 
 
 btnGenerate.addEventListener('click', (() => {
-
   getRandomValues();
 }));
 
 
+// function getRandomValues() {
+
+//   let minValueSum = +document.getElementById('min-value').value;
+//   let maxValueSum = +document.getElementById('max-value').value;
+//   let decimalPlaces = +document.getElementById('decimal-places').value;
+
+//   let thenValues = [];
+//   let randomSum = getRandomFloat(minValueSum, maxValueSum, decimalPlaces);
+//   let randomPayment = arrayRandElement(banknotes);
 
 
+//   if (randomPayment >= randomSum) {
 
+//     ifValues.map((elem) => {
+//       thenValues.push((elem - randomSum).toFixed(decimalPlaces));
+//     })
 
+//     getArrayFromNodeList(thenElements).map((elem, index) => {
+//       elem.innerHTML = `${thenValues[index]} евро`;
+//     });
+
+//     sum.innerHTML = `${randomSum} евро`;
+//     currentPayment.innerHTML = `${randomPayment} евро`; 
+    
+//   } else {
+//     getRandomValues();
+//   }
+
+// };
 
 function getRandomValues() {
 
@@ -43,38 +67,51 @@ function getRandomValues() {
   if (randomPayment >= randomSum) {
 
     ifValues.map((elem) => {
-      thenValues.push((elem - randomSum).toFixed(decimalPlaces));
+      sum.innerHTML = `${randomSum} евро`;
+      currentPayment.innerHTML = `${randomPayment} евро`;
+      
+      let randomSumTo005 roundTo005(randomSum);
+
+      thenValues.push((elem - randomSumTo005).toFixed(decimalPlaces));
     })
 
     getArrayFromNodeList(thenElements).map((elem, index) => {
       elem.innerHTML = `${thenValues[index]} евро`;
-    });
-
-    sum.innerHTML = `${randomSum} евро`;
-    currentPayment.innerHTML = `${randomPayment} евро`; 
-    
+    });   
   } else {
     getRandomValues();
   }
 
 };
 
-function getRandomFloat(min, max, decimal) {
-  return (Math.random() * (max - min) + min).toFixed(decimal);
-};
 
-function arrayRandElement(arr) {
-  var rand = Math.floor(Math.random() * arr.length);
-  return arr[rand];
-};
-
+//функция получает массив из нодлиста
 function getArrayFromNodeList(nodeList) {
   return Array.from(nodeList);
 };
 
+//функция получает data-атрибут "data" из каждого HTML элемента массива fromArray
+//и записывает в toArray
 function getDataAttributesFromElementsOfArray(fromArray, toArray) {
   fromArray.map(elem => {
     elem.getAttribute('data');
     toArray.push(elem.getAttribute('data'));
   })
 };
+
+//функция возвращает рандомное число в промежутке min-max и 
+//округляет его до decimal знаков после запятой
+function getRandomFloat(min, max, decimal) {
+  return (Math.random() * (max - min) + min).toFixed(decimal);
+};
+
+//фукция возвращает значение рандомного элемента массива
+function arrayRandElement(arr) {
+  var rand = Math.floor(Math.random() * arr.length);
+  return arr[rand];
+};
+
+//функция округляет значение числа до 0.05
+function roundTo005(num) {
+  return Math.round(num / 0.05) * 0.05;
+}
