1f16d8f879a4912821f067673408a61ba5d9e956
Iurrii <1513it@gmail.com>
2022-03-09 21:55:10 +0100

    улучшение логики, добавлено выделение текущей ситуации, исправлена вёрстка

--- script.js
+++ script.js
@@ -1,40 +1,60 @@
-let sum = document.querySelector(".sum");
-let currentPayment = document.querySelector(".current-payment");
+//пользовательские настройки и элементы управления
 let minValueSum = +document.getElementById("min-value").value;
 let maxValueSum = +document.getElementById("max-value").value;
 let decimalPlaces = +document.getElementById("decimal-places").value;
+let btnGenerate = document.querySelector(".generate-btn");
 
-let btnGenerate = document.querySelector(".block-inputs__btn");
+//точки вывода данных
+let sum = document.querySelector(".sum");
+let currentPayment = document.querySelector(".current-payment");
+
+//nodeList'ы
 let thenElements = document.querySelectorAll(".then");
 let ifElements = document.querySelectorAll(".if");
 
-let ifValues = [];
-let thenValues = [];
-let banknotes = [1, 2, 3, 4, 5, 10, 20, 50, 100];
+//JS переменные
+const ifValues = [];
+const thenValues = [];
+const banknotes = [1, 2, 3, 4, 5, 10, 20, 50, 100];
+let previousElement = null;
 
-getDataAttributesFromElementsOfArray(
-  getArrayFromNodeList(ifElements),
-  ifValues
-);
+
 
 btnGenerate.addEventListener("click", () => {
   getRandomValues();
 });
 
+//получает атрибуты элементов с классом if
+//записывает их в массив ifValues
+getDataAttributesFromElementsOfArray(
+  getArrayFromNodeList(ifElements),
+  ifValues
+);
+
+
+
 function getRandomValues() {
   let thenValues = [];
+  //сгенерировать рандомное значение суммы покупки согласно аргументам
   let randomSum = getRandomFloat(minValueSum, maxValueSum, decimalPlaces);
+  //сгенерировать рандомное значение оплаты клиента
   let randomPayment = arrayRandElement(banknotes);
+
+  //найти и изменить элемент
+  searchParentForElementWithData(ifElements, randomPayment);
 
   if (randomPayment >= randomSum) {
     ifValues.map((elem) => {
       let randomSumTo005 = roundTo005(randomSum);
-
+      //в массив thenValues записать округлённое значение выражения 
       thenValues.push((elem - randomSumTo005).toFixed(decimalPlaces));
-      sum.innerHTML = `${randomSum} евро`;
-      currentPayment.innerHTML = `${randomPayment} евро`;
     });
 
+    //вывести на экран рандомную сумму и рандомный платёж
+    sum.innerHTML = `${randomSum} евро`;
+    currentPayment.innerHTML = `${randomPayment} евро`;
+
+    //вывести на экран список элементов с содержимым из массива thenValues
     getArrayFromNodeList(thenElements).map((elem, index) => {
       elem.innerHTML = `${thenValues[index]} евро`;
     });
@@ -57,13 +77,42 @@ function getDataAttributesFromElementsOfArray(fromArray, toArray) {
   });
 }
 
+//функция ищет родителя у элемента из списка элементов с заданным data
+function searchParentForElementWithData(listOfElements, data) {
+  listOfElements.forEach((item) => {
+
+    let dataAttribute = +item.getAttribute("data");
+
+    if (dataAttribute === data) {
+
+      previousElement = item.parentElement;
+      //если текущий элемент != прошлому
+      if (item.parentElement !== previousElement) {
+        //переключить класс у найденного элемента
+        highlightingElementToggle(item.parentElement, "active-line");  
+        
+      } else {
+        item.parentElement.classList.add('active-line');
+      }
+
+    } else {
+      item.parentElement.classList.remove("active-line");
+    }
+  });
+}
+
+//переключает класс у элемента
+function highlightingElementToggle(element, className) {
+  element.classList.toggle(className);
+}
+
 //функция возвращает рандомное число в промежутке min-max и
 //округляет его до decimal знаков после запятой
 function getRandomFloat(min, max, decimal) {
   return (Math.random() * (max - min) + min).toFixed(decimal);
 }
 
-//фукция возвращает значение рандомного элемента массива
+//функция возвращает значение рандомного элемента массива
 function arrayRandElement(arr) {
   var rand = Math.floor(Math.random() * arr.length);
   return arr[rand];
